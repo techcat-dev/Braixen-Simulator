632fe396e1622a4163cff7049e7b82b31dd1788c
TechCat <143054835+techcat-dev@users.noreply.github.com>
2024-08-16 16:26:26 -0400

    Add files via upload

--- braixen-simulator.c
+++ braixen-simulator.c
@@ -1,32 +1,44 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <stdlib.h>
 
 int main () {
     int choice = 0;
     int dummy = 0;
+    int charm = 0;
     system("clear");
-    printf("a Wild Braixen has appeared! (Braixen Simulator, Pre-release Alpha 1.0) \n");
+    printf("_____________________________\n \n");
+    printf("a Wild Braixen has appeared! (Braixen Simulator, Pre-Alpha 1.1) \n");
     while (dummy == 0) {
+    printf("_____________________________\n \n");
+    printf("Charm Level: %d", charm);
+    printf("\n");
+    printf("_____________________________\n");
     printf("\nWhat will you do?:");
     printf("\n \n1. Pet her");
     printf("\n \n2. Throw a rock at her");
     printf("\n \n3. Try to feed her\n \n");
-    printf("4. Exit Game\n \n");
+    printf("4. Exit game\n");
+    printf("_____________________________\n \n");
     scanf("%d", &choice);
 
     if (choice == 1) {
         system("clear");
+        printf("_____________________________\n \n");
         printf("You've pet Braixen, she seems less mad now!\n");
     }
 
     else if (choice == 2) {
         system("clear");
-        printf("You tried throwing a rock at Braixen, but she avoided it!\n");
+        printf("_____________________________\n \n");
+        printf("You tried throwing a rock at Braixen, but she avoided it! She's super mad now.\n");
+        charm = 0;
     }
 
     else if (choice == 3) {
         system("clear");
-        printf("You tried feeding Braixen a PokeBerry, but she bit your hand instead!\n");
+        printf("_____________________________\n \n");
+        printf("You threw a PokeBerry near Braixen, she took it and ate it!\n");
+        charm++;
     }
 
     else if (choice == 4) {
@@ -35,7 +47,8 @@ int main () {
 
     else {
         system("clear");
-        printf("Please choose a valid option!");
+        printf("_____________________________\n \n");
+        printf("Please choose a valid option!\n");
     }
 
     }
