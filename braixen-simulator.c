ea4cc4a50d746f1ad0486bd53286d37493bab21b
TechCat <143054835+techcat-dev@users.noreply.github.com>
2024-08-17 13:28:34 -0400

    Update braixen-simulator.c

--- braixen-simulator.c
+++ braixen-simulator.c
@@ -1,7 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int firstlevel();
+int clearit();
+
 int main () {
+
+int userchoice = 0;
+
+clearit();
+
+printf(" ______              __                                 \n");
+printf("|   __ \\.----.---.-.|__|.--.--.-----.-----.             \n");
+printf("|   __ <|   _|  _  ||  ||_   _|  -__|     |             \n");
+printf("|______/|__| |___._||__||__.__|_____|__|__|             \n");
+printf("                                                        \n");
+printf(" _______ __                  __         __              \n");
+printf("|     __|__|.--------.--.--.|  |.---.-.|  |_.-----.----.\n");
+printf("|__     |  ||        |  |  ||  ||  _  ||   _|  _  |   _|\n");
+printf("|_______|__||__|__|__|_____||__||___._||____|_____|__|  \n");
+printf("                                                        \n \n");
+
+printf("1. Play Game \n");
+printf("2. Exit Game \n \n");
+
+printf("\n(Braixen Simulator Pre-Alpha 2.5 by TechCat-Dev on Github.) \n \n");
+
+
+scanf("%d", &userchoice);
+
+if (userchoice == 1) {
+    firstlevel();
+}
+
+else if (userchoice == 2) {
+clearit();    
+printf("\n(Braixen Simulator Pre-Alpha 2.5 by TechCat-Dev on Github.) \n \n");
+        }
+
+else {
+clearit();    
+printf("\n(Braixen Simulator Pre-Alpha 2.5 by TechCat-Dev on Github.) \n \n");
+        
+    }
+
+}
+
+
+
+
+
+
+int firstlevel () {
+
     //FIRST BOOT INT
     int choice = 0;
     int dummy = 0;
@@ -10,11 +61,8 @@ int main () {
     int berries = 5;
     int braixenhealth = 100;
     int exitq = 0;
-        #ifdef _WIN32
-            system("cls");
-        #else
-            system("clear");
-        #endif
+
+clearit();
             printf("_____________________________\n \n");
     printf("Current event: ");
     printf("a Wild Braixen has appeared!\n");    
@@ -24,12 +72,12 @@ int main () {
 
     //INT CHECKS
 
+        if (charm >= 100) {
+            charm = 100;
+        }
+
     if (userhealth == 0) {
-        #ifdef _WIN32
-            system("cls");
-        #else
-            system("clear");
-        #endif
+clearit();
         printf("_____________________________\n \n");
         printf("You lost all of your health, you blacked out!\n");
         printf("_____________________________\n \n");
@@ -45,24 +93,21 @@ int main () {
         berries = 5;
         braixenhealth = 100;
         exitq = 0;
-         #ifdef _WIN32
-            system("cls");
-        #else
-            system("clear");
-        #endif
+clearit();
+    printf("_____________________________\n \n");
+    printf("Current event: ");
+    printf("a Wild Braixen has appeared!\n");
         }
         else {
+        clearit();    
+        printf("\n(Braixen Simulator Pre-Alpha 2.5 by TechCat-Dev on Github.) \n \n");
         return 0;
         }
     }
 
 
     if (braixenhealth == 0) {
-        #ifdef _WIN32
-            system("cls");
-        #else
-            system("clear");
-        #endif        
+clearit();
         printf("_____________________________\n \n");
         printf("Braixen lost all of her health, she fainted!\n");
         printf("_____________________________\n \n");
@@ -78,20 +123,20 @@ int main () {
         berries = 5;
         braixenhealth = 100;
         exitq = 0;
-         #ifdef _WIN32
-            system("cls");
-        #else
-            system("clear");
-        #endif
+         clearit();
+    printf("_____________________________\n \n");
+    printf("Current event: ");
+    printf("a Wild Braixen has appeared!\n");
         }
         else {
+        clearit();    
+        printf("\n(Braixen Simulator Pre-Alpha 2.5 by TechCat-Dev on Github.) \n \n");
         return 0;
         }
     }
     
     //MAIN MENU
-    printf("_____________________________\n");
-    printf("\n(Braixen Simulator Pre-Alpha 2.1 by TechCat-Dev on Github.) \n \n");
+    printf("_____________________________\n \n");
     printf("Braixen's Stats: \n");
     printf("Charm Level: %d", charm);
     printf("\n");
@@ -103,22 +148,18 @@ int main () {
     printf("Berries Left: %d", berries);
     printf("\n");
     printf("_____________________________\n");
-    printf("\nWhat will you do?:");
-    printf("\n \n1. Pet her");
-    printf("\n \n2. Throw a rock at her");
-    printf("\n \n3. Try to feed her\n \n");
-    printf("4. Exit game\n");
+    printf("\nWhat will you do?: \n");
+    printf("\n1. Pet her");
+    printf("\n2. Throw a rock at her");
+    printf("\n3. Try to feed her");
+    printf("\n4. Exit game\n");
     printf("_____________________________\n \n");
     scanf("%d", &choice);
 
     //PROCESSES USER CHOICES
 
     if (choice == 1) {
-        #ifdef _WIN32
-            system("cls");
-        #else
-            system("clear");
-        #endif       
+        clearit();       
          if (charm < 25) {
             printf("_____________________________\n \n");
             printf("Current event: ");
@@ -126,21 +167,18 @@ int main () {
             userhealth = userhealth - 10;
         }
 
-        if (charm >= 25) {
+        else if (charm >= 25) {
         printf("_____________________________\n \n");
         printf("Current event: ");
         printf("You've pet Braixen, she seems less mad now!\n");
-        charm++;
+        charm = charm + 10;
         }
+
 
     }
 
     else if (choice == 2) {
-        #ifdef _WIN32
-            system("cls");
-        #else
-            system("clear");
-        #endif        
+clearit();
         printf("_____________________________\n \n");
         printf("Current event: ");
         printf("You threw a rock at Braixen, it hit her head! you made her cry.\n");
@@ -150,11 +188,7 @@ int main () {
 
     else if (choice == 3) {        
         if (berries > 0) {
-        #ifdef _WIN32
-            system("cls");
-        #else
-            system("clear");
-        #endif        
+clearit();
         printf("_____________________________\n \n");
         printf("Current event: ");
         printf("You threw a PokeBerry near Braixen, she took it and ate it!\n");
@@ -163,28 +197,23 @@ int main () {
         }
 
         else if (berries == 0) {
-        #ifdef _WIN32
-            system("cls");
-        #else
-            system("clear");
-        #endif        
+clearit();
         printf("_____________________________\n \n");
         printf("Current event: ");
         printf("You're out of berries!\n");
         }
 
+
     }
 
     else if (choice == 4) {
-        dummy = 1;
+        clearit();    
+        printf("\n(Braixen Simulator Pre-Alpha 2.5 by TechCat-Dev on Github.) \n \n");
+        return 0;
     }
 
     else {
-        #ifdef _WIN32
-            system("cls");
-        #else
-            system("clear");
-        #endif        
+clearit();
         printf("_____________________________\n \n");
         printf("Current event: ");
         printf("Please choose a valid option!\n");
@@ -193,4 +222,12 @@ int main () {
     }
 
 
+}
+
+int clearit () {
+    #ifdef _WIN32
+            system("cls");
+        #else
+            system("clear");
+        #endif
 }
